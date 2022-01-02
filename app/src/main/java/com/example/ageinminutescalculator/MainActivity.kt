c96c28a7aef74e484e9b88660367a0ab18141f2e
jholgadm <jholgadm@emeal.nttdata.com>
2022-01-02 21:15:04 +0100

    - Button redesign select day - Added counter in minutes - Added Spanish traslation - You can only pick days in the past or one day in the future from the current day - Solve some issues

--- app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutescalculator/MainActivity.kt
@@ -1,39 +1,71 @@
 package com.example.ageinminutescalculator
 
 import android.app.DatePickerDialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tv_Date: TextView? = null
+    private var tv_Minutes: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btn_DateSelect.setOnClickListener {
+        val btnDateSelect: Button = findViewById(R.id.btnDateSelect)
+        tv_Date = findViewById(R.id.tv_Date)
+        tv_Minutes = findViewById(R.id.tv_Minutes)
+
+        btnDateSelect.setOnClickListener {
             clickDatePicker()
         }
 
     }
 
-    fun clickDatePicker() {
+    private fun clickDatePicker() {
 
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+            this, { _, selectedYear, selectedMonth, selectedDayOfMonth ->
+                Toast.makeText(this, getString(R.string.selection_done), Toast.LENGTH_SHORT).show()
 
-        DatePickerDialog(this,
-            DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
-                Toast.makeText(this, "Button pressed", Toast.LENGTH_SHORT).show()
+                val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+                tv_Date?.text = selectedDate
+
+                val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+                val theDate = sdf.parse(selectedDate)
+                theDate?.let {
+
+                    val selectedDateInMinutes = theDate.time / 6000
+
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+
+                        val currentDayInMinutes = currentDate.time / 6000
+                        val differenceInMinutes = currentDayInMinutes - selectedDateInMinutes
+
+                        tv_Minutes?.text = differenceInMinutes.toString()
+                    }
+
+                }
+
             },
             year,
             month,
             day
-            ).show()
+        )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
 
     }
 }
